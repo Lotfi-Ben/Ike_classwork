aa36636c5cb22c93f023a94763e0312ef8114b5a
LOTFI BENCHIKH <513257@my.cwc.ac.uk>
2025-09-30 10:33:30 +0100

    spinbox

--- EXERCISE/Label_Exercise.py
+++ EXERCISE/Label_Exercise.py
@@ -5,25 +5,25 @@ def set_label():
     edited_label = label.cget("text") + "_edited"
     output_string.set(edited_label)
 
-# Window
+#window
 window = tk.Tk()
 window.title("Label")
 window.geometry("400x400")
 
-# Style
+#style
 style = ttk.Style()
 style.configure("Custom.TLabel", foreground="red", font=("Comic Sans MS", 24))
 
-# Label
+#label
 label = ttk.Label(window, text="Label", style="Custom.TLabel", justify="center")
 label.configure(underline=0)
 label.pack(pady=20)
 
-# Button
+#button
 button = ttk.Button(window, text="Edit", command=set_label)
 button.pack(pady=10)
 
-# Output
+#output
 output_string = tk.StringVar()
 output_label = ttk.Label(window, style="Custom.TLabel", textvariable=output_string)
 output_label.pack(pady=10)
